a4145232bd11f7bec522cc0464c7afe93d19d294
cgg <891842749@qq.com>
2018-05-08 23:45:10 +0800

    添加controller

---
+++ src/cn/jxufe/controller/CodeCropStatusController.java
@@ -0,0 +1,22 @@
+package cn.jxufe.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.jxufe.entity.CodeCropStatus;
+import cn.jxufe.service.CodeCropStatusService;
+@Controller
+@RequestMapping("/codeCropStatus")
+	public class CodeCropStatusController {
+	    @Autowired
+	    private CodeCropStatusService codeCropStatusService;
+	    @RequestMapping(value="/data",produces=MediaType.APPLICATION_JSON_VALUE)
+	    @ResponseBody
+	    public Iterable<CodeCropStatus> data() throws Exception {
+	        return codeCropStatusService.findALl();        
+	    }
+	}
+
---
+++ src/cn/jxufe/controller/CropsGrowController.java
@@ -0,0 +1,34 @@
+package cn.jxufe.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.jxufe.bean.Message;
+import cn.jxufe.entity.CropsGrow;
+import cn.jxufe.service.CropsGrowService;
+
+@Controller
+	@RequestMapping("cropsGrow")
+	public class CropsGrowController {
+	    @Autowired
+	    private CropsGrowService cropsGrowService;
+	    @RequestMapping(value="grid")
+	    public String grid(){
+	        return "cropsGrow/grid";
+	    }
+	    @RequestMapping(value="/gridData",produces=MediaType.APPLICATION_JSON_VALUE)
+	    @ResponseBody
+	    public List<CropsGrow> gridData(int cId) throws Exception {
+	        return cropsGrowService.findSome(cId);    
+	    }
+	    @RequestMapping(value="save",produces=MediaType.APPLICATION_JSON_VALUE)
+	    @ResponseBody
+	    public Message save(CropsGrow cropsGrow,Model model){       
+	        return cropsGrowService.save(cropsGrow);
+	    }
+	}

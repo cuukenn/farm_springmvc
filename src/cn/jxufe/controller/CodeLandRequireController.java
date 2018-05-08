0a02a221bcc4041d375444c2d1cee7507c9967d2
cgg <891842749@qq.com>
2018-05-08 23:45:10 +0800

    添加controller

---
+++ src/cn/jxufe/controller/CodeLandRequireController.java
@@ -0,0 +1,22 @@
+package cn.jxufe.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.jxufe.entity.CodeLandRequire;
+import cn.jxufe.service.CodeLandRequireService;
+@Controller
+@RequestMapping("/codeLandRequire")
+	public class CodeLandRequireController {
+	    @Autowired
+	    private CodeLandRequireService codeLandRequireService;
+	    @RequestMapping(value="/data",produces=MediaType.APPLICATION_JSON_VALUE)
+	    @ResponseBody
+	    public Iterable<CodeLandRequire> data() throws Exception {
+	        return codeLandRequireService.findALl();        
+	    }
+	}
+

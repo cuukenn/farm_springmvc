91c09f97180e8cfb2d3774967559f530f46d39b1
Administrator <Administrator@10.1.1.105>
2018-05-17 20:17:55 +0800

    写好了FileSaver

--- src/cn/jxufe/utils/FileSaver.java
+++ src/cn/jxufe/utils/FileSaver.java
@@ -1,16 +1,26 @@
 package cn.jxufe.utils;
 
+import java.io.File;
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.web.multipart.MultipartFile;
 
 import cn.jxufe.bean.Message;
 
 public class FileSaver {
-
-	public static Message save(String string, HttpServletRequest request, MultipartFile uploadFile) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Message save(String imagesPath,HttpServletRequest request,MultipartFile file) {
+		Message message = new Message();
+		if (!file.isEmpty()) {  
+            try {
+                String filePath = request.getSession().getServletContext().getRealPath("/") +imagesPath  
+                        + file.getOriginalFilename();  
+                file.transferTo(new File(filePath));
+                message.setCode(0);
+                message.setMsg("保存成功");
+            } catch (Exception e) {  
+                message.setCode(-1001);
+                message.setMsg("保存失败");
+            }  
+        } 
+		return message;
 	}
-
 }

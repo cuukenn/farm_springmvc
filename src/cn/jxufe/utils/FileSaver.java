9f2de472eb778038e9e75b64756b336ad1d133af
Administrator <Administrator@10.1.1.80>
2018-05-17 20:00:40 +0800

    controller

---
+++ src/cn/jxufe/utils/FileSaver.java
@@ -0,0 +1,16 @@
+package cn.jxufe.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import cn.jxufe.bean.Message;
+
+public class FileSaver {
+
+	public static Message save(String string, HttpServletRequest request, MultipartFile uploadFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

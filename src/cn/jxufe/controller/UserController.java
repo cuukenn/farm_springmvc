f58753bb784b89f306b8876efd5fd68ab98630a8
Administrator <Administrator@10.1.2.241>
2018-05-24 19:18:32 +0800

    farmController

--- src/cn/jxufe/controller/UserController.java
+++ src/cn/jxufe/controller/UserController.java
@@ -40,8 +40,6 @@ public class UserController {
 	 * @see cn.jxufe.service.UserService
 	 */
 	private UserService userService;
-	private FarmService farmService;
-
 	@RequestMapping(value = "grid")
 	/**
 	 * 
@@ -108,9 +106,4 @@ public class UserController {
 		return userService.delete(user);
 	}
 
-	@RequestMapping(value = "setCurUser", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public Message setCurUser(HttpSession session, @RequestBody User user) {
-		return farmService.setCurUser(session, user);
-	}
 }
